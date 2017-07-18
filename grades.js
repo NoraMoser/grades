ed924e6e046755dbbbadc30e2d20a43f59cfcc03
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-07-18 15:48:19 -0500

    still more to do

---
+++ grades.js
@@ -0,0 +1,16 @@
+let scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+// console.log(Math.max(...scores));
+// console.log(Math.min(...scores));
+
+let F = [];
+let D = [];
+let C = [];
+let B = [];
+let A = [];
+
+
+if (scores > 50 && < 60) {
+	grade = "F";
+}
+
+console.log(grade);
