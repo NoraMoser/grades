11a63f6be137a6ac129fd4a2e69e90024887fca1
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-07-19 14:12:20 -0500

    finished javascript

--- grades.js
+++ grades.js
@@ -2,19 +2,51 @@ let scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 // console.log(Math.max(...scores));
 // console.log(Math.min(...scores));
 
-let F = [];
-let D = [];
-let C = [];
-let B = [];
-let A = [];
+// let F = [];
+// let D = [];
+// let C = [];
+// let B = [];
+// let A = [];
 
 let currentGrade = "";
+let F = 0;
+let D = 0;
+let C = 0;
+let B = 0;
+let A = 0;
 
-function letterGrades(grades) {
-	for (let i = 0; i < grades.length; i++) {
+// function letterGrades(grades) {
+	for (let i = 0; i < scores.length; i++) {
 		// console.log(grades);
-		let currentGrade = grades[i];
-		if (grade > 50 && < 60);
-		F.push(currentGrade);
+		currentGrade = scores[i];
+		// console.log(currentGrade);
+		if (currentGrade >= 50 && currentGrade <= 60) {
+			F += 1
+			
+		} else if (currentGrade >= 61 && currentGrade <= 70) {
+			
+			D += 1 
+			
+		} else if (currentGrade >= 71 && currentGrade <= 80) {
+			
+			C += 1 
+		} else if (currentGrade >= 81 && currentGrade <= 90) {
+			
+			B += 1
+
+		} else if (currentGrade >= 91 && currentGrade <= 100) {
+			
+			A += 1 
+			// console.log(letterGrade);
+		}
 	}
-}
+
+console.log("There are " + A + " As.");
+console.log("There are " + B + " Bs.");
+console.log("There are " + C + " Cs.");
+console.log("There are " + D + " Ds.");
+console.log("There are " + F + " Fs.");
+console.log(Math.max(...scores));
+console.log(Math.min(...scores));
+
+// letterGrades(scores);
